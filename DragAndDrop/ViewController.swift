b04881980d6c22de18e7a37530989121c9330813
Nikolas Omelianov <nikolas@Nikolass-Mac-mini.local>
2018-02-07 19:28:04 +0200

    Initial Commit

---
+++ DragAndDrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragAndDrop
+//
+//  Created by Nikolas Omelianov on 07.02.18.
+//  Copyright © 2018 Nikolas Omelianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
